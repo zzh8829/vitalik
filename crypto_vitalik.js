393e89927b55160ddf6a89f4c55ea2344f68f764
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-09 13:14:41 -0400

    express

--- crypto_vitalik.js
+++ crypto_vitalik.js
@@ -145,3 +145,14 @@ login(credentials, (err, api) => {
     }
   });
 });
+
+
+var express = require('express');
+var app     = express();
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
