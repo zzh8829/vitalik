bcfa68a99c3fc9063ab8468e7f23f2ee06360aef
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-06-09 13:20:49 -0400

    cleaned up express stuff

--- crypto_vitalik.js
+++ crypto_vitalik.js
@@ -146,13 +146,11 @@ login(credentials, (err, api) => {
   });
 });
 
-
+// We listen on whatever port Heroku sets for us so that the dyno doesn't crash.
+// Read: This is the most hacky code I've ever written.
 var express = require('express');
 var app     = express();
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'));
-});
+  response.send('Application is running.');
+}).listen(app.get('port'), function() {});
